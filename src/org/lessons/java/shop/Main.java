1d2db00a35a45c89372a41a59c2902598b02c0f7
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-06 20:24:57 +0200

    add methods and test in Main

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,9 +1,20 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
 
-        Product pluto = new Product();
-        Product pippo = new Product();
+        // String name = "pluto";
+        String description = "Figurina del cane di Paperino";
+        BigDecimal iva = new BigDecimal(0.22);
+        BigDecimal price = new BigDecimal(100);
+
+        Product pluto = new Product("pluto", description, iva, price);
+
+        System.out.println("price = " + pluto.getPrice());
+        System.out.println("price with iva = " + pluto.getPricePlusIva());
+        System.err.println(pluto.getSlug());
+
     }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,18 +1,46 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 
 public class Product {
     public String name;
     public String description;
     public int code;
-    public double iva;
-    public double price;
+    public BigDecimal iva;
+    public BigDecimal price;
 
-    Product() {
+    // Alla creazione di un nuovo prodotto, inizializzo il codice con un numero
+    // intero random (compreso tra 0(incluso) e max (non incluso))
+    public Product(String name, String description, BigDecimal iva, BigDecimal price) {
         Random r = new Random();
-        int max = 100;
+        int max = 10000000;
         this.code = r.nextInt(max);
         // System.out.println(this.code);
+
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+    // metodo per ottenere il prezzo base del prodotto
+    public BigDecimal getPrice() {
+        return this.price;
+    }
+
+    // metodo per ottenere il prezzo comprensivo di iva del prodotto
+    public BigDecimal getPricePlusIva() {
+        // verificare se sono state inizializzate le varibaili di istanza
+        if (price != null && iva != null) {
+            return this.price.add(price.multiply(iva)).setScale(2, RoundingMode.DOWN);
+        }
+        return null;
+    }
+
+    // metodo per avere il nome esteso
+    public String getSlug() {
+        return this.name + "-" + this.code;
     }
 }

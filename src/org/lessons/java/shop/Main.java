5e748f6381bf2d5ad2703faf5107ed088091ff88
saverio mandarano <saverio.mandarano1@gmail.com>
2026-04-07 17:34:41 +0200

    Aggiunti tutti i getter per le variabili di istanza

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -5,16 +5,24 @@ import java.math.BigDecimal;
 public class Main {
     public static void main(String[] args) {
 
-        // String name = "pluto";
         String description = "Figurina del cane di Paperino";
         BigDecimal iva = new BigDecimal(0.22);
-        BigDecimal price = new BigDecimal(100);
+        BigDecimal price = new BigDecimal(100.015);
 
-        Product pluto = new Product("pluto", description, iva, price);
+        Product pluto = new Product("pluto", "Nike", description, iva, price);
 
+        // getter
+        System.out.println("name = " + pluto.getName());
+        System.out.println("code = " + pluto.getCode());
+        System.out.println("slug = " + pluto.getSlug());
+        System.out.println("description = " + pluto.getDescription());
+        System.out.println("brand = " + pluto.getBrand());
+        System.out.println("iva = " + pluto.getIva());
         System.out.println("price = " + pluto.getPrice());
         System.out.println("price with iva = " + pluto.getPricePlusIva());
-        System.err.println(pluto.getSlug());
+
+        // setter
 
     }
+
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -5,32 +5,59 @@ import java.math.RoundingMode;
 import java.util.Random;
 
 public class Product {
-    public String name;
-    public String description;
-    public int code;
-    public BigDecimal iva;
-    public BigDecimal price;
+    private String name;
+    private String description;
+    private String brand;
+    private int code;
+    private BigDecimal iva;
+    private BigDecimal price;
 
-    // Alla creazione di un nuovo prodotto, inizializzo il codice con un numero
-    // intero random (compreso tra 0(incluso) e max (non incluso))
-    public Product(String name, String description, BigDecimal iva, BigDecimal price) {
+    public Product(String name, String brand, String description, BigDecimal iva, BigDecimal price) {
+        // alla creazione nuovo prodotto il codice è valorizzato con un numero random
         Random r = new Random();
         int max = 10000000;
         this.code = r.nextInt(max);
-        // System.out.println(this.code);
 
         this.name = name;
         this.description = description;
+        this.brand = brand;
         this.price = price;
         this.iva = iva;
     }
 
-    // metodo per ottenere il prezzo base del prodotto
-    public BigDecimal getPrice() {
-        return this.price;
+    // getter
+    public String getName() {
+        return this.name;
     }
 
-    // metodo per ottenere il prezzo comprensivo di iva del prodotto
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public int getCode() {
+        return this.code;
+    }
+
+    public BigDecimal getIva() {
+        if (iva != null) {
+            return this.iva.setScale(2, RoundingMode.DOWN);
+        }
+        return null;
+    }
+
+    // metodo per avere il prezzo base
+    public BigDecimal getPrice() {
+        if (price != null) {
+            return this.price.setScale(2, RoundingMode.DOWN);
+        }
+        return null;
+    }
+
+    // metodo per avere il prezzo comprensivo di iva
     public BigDecimal getPricePlusIva() {
         // verificare se sono state inizializzate le varibaili di istanza
         if (price != null && iva != null) {
@@ -39,8 +66,14 @@ public class Product {
         return null;
     }
 
-    // metodo per avere il nome esteso
+    // metodo per avere il nome esteso, ottenuto concatenando codice-nome
     public String getSlug() {
-        return this.name + "-" + this.code;
+        if (code != 0 && name != null) {
+            return this.code + "-" + this.name;
+        }
+        return null;
     }
+
+    // setter
+
 }
